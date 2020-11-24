08d752f32177baf5d4c30feb76db1db8da2564d4
Tomomi <58757939+tomi-design@users.noreply.github.com>
2020-11-24 00:49:38 -0500

    Used css variables to make dark mode switch possible

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let darkmode = localStorage.getItem('darkmode');
+
+const darkmodeToggle = document.getElementById('chk');
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkmode', 'enabled');
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode');
+  localStorage.setItem('darkmode', null);
+}
+
+if(darkmode === 'enabled'){
+  enableDarkmode();
+}
+
+darkmodeToggle.addEventListener('click', ()=> {
+  darkmode = localStorage.getItem('darkmode');
+
+  if(darkmode !== 'enabled'){
+    enableDarkmode();
+  } else {
+    disableDarkmode();
+  }
+});
