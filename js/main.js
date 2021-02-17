1d594027159091482728e118ea69545dbe89e0d2
Tomomi <58757939+tomi-design@users.noreply.github.com>
2021-02-17 15:05:07 -0500

    Added commented dividers for each section in html and cleaned unused code in css

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
 let darkmode = localStorage.getItem('darkmode');
-// let mode = document.getElementById('mode');
-
-
 const darkmodeToggle = document.getElementById('chk');
+
+
+//Functions to enable and disable dark mode
 
 const enableDarkmode = () => {
   document.body.classList.add('darkmode');
   localStorage.setItem('darkmode', 'enabled');
-  // mode.innerHTML = 'Light Mode';
-
 }
 
 const disableDarkmode = () => {
   document.body.classList.remove('darkmode');
   localStorage.setItem('darkmode', null);
-  // mode.innerHTML = 'Dark Mode';
-
 }
 
 if(darkmode === 'enabled'){
   enableDarkmode();
 }
 
+//Dark / Light mode toggle button 
 darkmodeToggle.addEventListener('click', ()=> {
   darkmode = localStorage.getItem('darkmode');
 
